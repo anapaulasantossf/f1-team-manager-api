0144df38294cfa9486e8f4d9793f800f1fc5682a
anapaulasantossf <anapaulasantossf@gmail.com>
2025-05-23 19:57:48 -0300

    Subir estrutura inicial do  projeto

---
+++ src/main/java/br/com/anapaulasantossf/f1_team_manager_api/F1TeamManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.anapaulasantossf.f1_team_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1TeamManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1TeamManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/anapaulasantossf/f1_team_manager_api/F1TeamManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anapaulasantossf.f1_team_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1TeamManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
