87f2afdff81a581ec15b7d148b54b5da9c67f4a2
anapaulasantossf <anapaulasantossf@gmail.com>
2025-05-29 00:13:48 -0300

    feat: Criar restante das rotas de employees

--- src/main/java/br/com/anapaulasantossf/f1_team_manager_api/service/EmployeeService.java
+++ src/main/java/br/com/anapaulasantossf/f1_team_manager_api/service/EmployeeService.java
@@ -4,9 +4,16 @@ import br.com.anapaulasantossf.f1_team_manager_api.dtos.EmployeeRequestDTO;
 import br.com.anapaulasantossf.f1_team_manager_api.dtos.EmployeeResponseDTO;
 import br.com.anapaulasantossf.f1_team_manager_api.model.Employee;
 import br.com.anapaulasantossf.f1_team_manager_api.repository.EmployeeRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
@@ -20,6 +27,44 @@ public class EmployeeService {
     public EmployeeResponseDTO create(EmployeeRequestDTO employeeRequestDTO) {
         Employee employee = employeeRepository.save(toEntity(employeeRequestDTO));
         return toDTO(employee);
+    }
+
+    @Transactional(readOnly = true) // Boa prática para operações de leitura
+    public List<EmployeeResponseDTO> findAll() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream()
+                .map(this::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<EmployeeResponseDTO> findById(UUID id) {
+        return Optional.ofNullable(employeeRepository.findById(id)
+                .map(this::toDTO)
+                .orElseThrow(() -> new EntityNotFoundException("Endereço não encontrado " + id)));
+    }
+
+    @Transactional
+    public EmployeeResponseDTO update(UUID id, EmployeeRequestDTO employeeRequestDTO) {
+        Optional<Employee> employeeOptional = employeeRepository.findById(id);
+        if (employeeOptional.isPresent()) {
+            Employee existingEmployee = employeeOptional.get();
+            modelMapper.map(employeeRequestDTO, existingEmployee);
+            Employee updatedEmployee = employeeRepository.save(existingEmployee);
+            return toDTO(updatedEmployee);
+        } else {
+            return null;
+        }
+    }
+
+    @Transactional
+    public boolean deleteById(UUID id) {
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private Employee toEntity(EmployeeRequestDTO employeeRequestDTO) {
